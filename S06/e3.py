15191ec3bfecf7b4fdcfc6876eff5f8511846640
adrianak <a.kruse.2024@alumnos.urjc.es>
2026-02-25 14:26:49 +0100

    a

---
+++ S06/e3.py
@@ -0,0 +1,36 @@
+
+class Seq:
+    def __init__(self, seq_list):
+        self.seq_list = seq_list
+
+    def __str__(self):
+        """Method called when the object is being printed"""
+        return self.seq_list
+
+    def __len__(self):
+        return len(self.seq_list)
+
+def generate_seqs(pattern, number):
+    new_list = []
+    for seq in range(number):
+        number_seq = pattern * (seq + 1)
+        new_list.append(Seq(number_seq))
+    return new_list
+
+def print_seqs(seq_list):
+    index = 0
+    for seq in seq_list:
+        print(f"Sequence {index}: (Length: {len(seq)}) {seq}")
+        index += 1
+
+
+seq_list1 = generate_seqs("A", 3)
+seq_list2 = generate_seqs("AC", 5)
+
+print("List 1:")
+print_seqs(seq_list1)
+
+print()
+print("List 2:")
+print_seqs(seq_list2)
+
