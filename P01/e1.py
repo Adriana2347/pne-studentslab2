2bce2d8e3a08193f7303e9c71876b8b83d972c00
adrianak <a.kruse.2024@alumnos.urjc.es>
2026-03-03 14:25:52 +0100

    a

--- P01/e1.py
+++ P01/e1.py
@@ -1,7 +1,7 @@
 from Seq1 import Seq
 
 def print_seqs(seq):
-    print(f"Sequence: (Length: {len(seq)}) {seq}")
+    print(f"Sequence: (Length: {seq.len()}) {seq}")
 
 s1 = Seq("ACTGA")
 print_seqs(s1)
