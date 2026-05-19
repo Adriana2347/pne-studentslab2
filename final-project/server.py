87f6464a7e82fd3b35aac14670e78690cbec7be7
adrianak <a.kruse.2024@alumnos.urjc.es>
2026-05-19 17:53:16 +0200

    a

--- final-project/server.py
+++ final-project/server.py
@@ -32,12 +32,21 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
         content = j.Template(contents)
         return content
 
+    def check_json(self, arguments):
+        return arguments.get("json", ["0"])[0] == "1"
+
+    def send_json(self, data, code=200):
+        self.send_response(code)
+        self.send_header('Content-Type', 'application/json')
+        self.end_headers()
+        self.wfile.write(json.dumps(data).encode())
+
     def do_GET(self):
 
         url_path = urlparse(self.path)
         path = url_path.path
         arguments = parse_qs(url_path.query)
-        print(arguments)
+        json_mode = self.check_json(arguments)
 
         body = "ninguna de las anteriores"
 
@@ -49,25 +58,35 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(200)
 
         elif path == "/listSpecies":
-            limit = arguments["limit"][0]
-            if limit is None or limit == "":
-                new_limit = None
-            else:
-                new_limit = int(limit)
-                url = "/info/species"
-                data = client_request(url)
-                species = json.loads(data)
-                species_list = []
-                for specie in species["species"]:
-                    if "common_name" in specie:
-                        species_list.append(specie["common_name"])
-                    else:
-                        species_list.append(specie["name"])
-                    if new_limit is not None and len(species_list) >= new_limit:
-                        break
-                species_list = ", ".join(species_list)
-                body = self.read_html_file("list_species.html").render(context={"Sequence": species_list, "Limit": new_limit})
-                self.send_response(200)
+            try:
+                limit = arguments["limit"][0]
+                if limit is None or limit == "":
+                    new_limit = None
+                else:
+                    new_limit = int(limit)
+                    url = "/info/species"
+                    data = client_request(url)
+                    species = json.loads(data)
+                    species_list = []
+                    for specie in species["species"]:
+                        if "common_name" in specie:
+                            species_list.append(specie["common_name"])
+                        else:
+                            species_list.append(specie["name"])
+                        if new_limit is not None and len(species_list) >= new_limit:
+                            break
+                    species_list = ", ".join(species_list)
+
+                    if json_mode:
+                        return self.send_json({"Sequence": species_list, "Limit": new_limit})
+
+                    body = self.read_html_file("list_species.html").render(context={"Sequence": species_list, "Limit": new_limit})
+                    self.send_response(200)
+
+            except Exception as error:
+                body = self.read_html_file("error.html").render(
+                    context={"error": str(error)})
+                self.send_response(500)
 
         elif path == "/karyotype":
             try:
@@ -78,6 +97,9 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                 if "karyotype" in  name_chromosomes:
                     chromosome_list = name_chromosomes["karyotype"]
                     chromosome_list = ", ".join(chromosome_list)
+                    if json_mode:
+                        return self.send_json({"Chromosome": chromosome_list})
+
                     body = self.read_html_file("chromosomes.html").render(context={"Chromosome": chromosome_list})
                     self.send_response(200)
                 else:
@@ -109,6 +131,9 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                             context={"error": "Chromosome not found"})
                         self.send_response(404)
                     else:
+                        if json_mode:
+                            return self.send_json({"length": length})
+
                         body = self.read_html_file("chromo_l.html").render(context={"length": length})
                         self.send_response(200)
 
@@ -125,6 +150,8 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                 gene_id = json.loads(data)
                 if "id" in gene_id:
                     id_gene = gene_id["id"]
+                    if json_mode:
+                        return self.send_json({"id": id_gene, "gene":gene})
                     body = self.read_html_file("gene_id.html").render(context={"id": id_gene, "gene":gene})
                     self.send_response(200)
                 else:
@@ -149,6 +176,8 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                     seq = json.loads(data2)
                     if "seq" in seq:
                         sequence = seq["seq"]
+                        if json_mode:
+                            return self.send_json({"sequence": sequence, "gene": gene})
                         body = self.read_html_file("geneSeq.html").render(context={"sequence": sequence, "gene": gene})
                         self.send_response(200)
                     else:
@@ -184,6 +213,8 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                         chromosome = gene_info["seq_region_name"]
                     if start is not None and end is not None:
                         length = end - start
+                        if json_mode:
+                            return self.send_json({"end": end, "start": start, "chromosome": chromosome, "length": length})
                         body = self.read_html_file("gene_info.html").render(context={"end": end, "start": start, "chromosome": chromosome, "length": length})
                         self.send_response(200)
                 else:
@@ -193,8 +224,6 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                 body = self.read_html_file("error.html").render(
                     context={"error": str(error)})
                 self.send_response(500)
-
-
 
         elif path == "/geneCalc":
             try:
@@ -215,6 +244,8 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                         porcentage = {}
                         for base, quantity in total_bases.items():
                             porcentage[base] = round((quantity / total_len) * 100, 2)
+                        if json_mode:
+                            return self.send_json({"length": total_len, "bases": total_bases, "percentage": porcentage})
                         body = self.read_html_file("gene_calc.html").render(context={"length": total_len, "bases": total_bases, "percentage": porcentage})
                         self.send_response(200)
                 else:
@@ -243,6 +274,8 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                             gene_name = gen["external_name"]
                             result_list.append(gene_name)
                         result_list = ", ".join(result_list)
+                        if json_mode:
+                            return self.send_json({"result_list": result_list})
                         body = self.read_html_file("gene_list.html").render(context={"result_list": result_list})
                         self.send_response(200)
             except Exception as error:
